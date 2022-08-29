14f067a67cc7ae0715320220f6020c99c425ae6f
Garrett D'Amore <garrett@damore.org>
2022-08-29 10:34:50 -0700

    Add attr and style modules.

---
+++ source/dcell/attr.d
@@ -0,0 +1,22 @@
+// Copyright 2022 Garrett D'Amore
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or https://www.boost.org/LICENSE_1_0.txt)
+
+module source.dcell.attr;
+
+/** 
+ * Text attributes that do not include color.
+ */
+enum Attr : int
+{
+    none = 0, /// normal, plain text
+    bold = 1 << 0,
+    blink = 1 << 1,
+    reverse = 1 << 2, /// foreground and background colors reversed
+    underline = 1 << 3,
+    dim = 1 << 4,
+    italic = 1 << 5,
+    strikethrough = 1 << 6,
+    invalid = -1, // invalid attribute
+}
---
+++ source/dcell/style.d
@@ -0,0 +1,22 @@
+// Copyright 2022 Garrett D'Amore
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or https://www.boost.org/LICENSE_1_0.txt)
+
+module dcell.style;
+
+public import dcell.attr;
+public import dcell.color;
+
+/// Text styling, which is an aggregate of attributes like bold or reverse,
+/// coloring (both foreground and background) and other attributes such as
+/// being clickable (to open a URL for example).
+struct Style {
+    Color fg; /// foreground color
+    Color bg; /// background color
+    Attr attr; /// text attributes
+    string url; /// clickable URL, or none if empty
+}
+
+// NB: tcell uses an opaque class/data structure for this, but for dcell
+// it seems more idiomatic to just expose the structure members directly.
