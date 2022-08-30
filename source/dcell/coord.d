9722ecc9d385897684a47bf40a658d1d0296fb6a
Garrett D'Amore <garrett@damore.org>
2022-08-30 14:06:00 -0700

    Coord so that we can pass x and y in a single parameter.

---
+++ source/dcell/coord.d
@@ -0,0 +1,15 @@
+// Copyright 2022 Garrett D'Amore
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or https://www.boost.org/LICENSE_1_0.txt)
+
+module dcell.coord;
+
+/** 
+ * Coordinates are X, Y values.
+ */
+struct Coord
+{
+    int x; // aka column
+    int y; // aka row
+}
