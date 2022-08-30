f835fd8e9f31d1b09602e223ce601ae95973fd59
Garrett D'Amore <garrett@damore.org>
2022-08-30 14:06:20 -0700

    Use coordinates, and operator overloading to make it cleaner.

--- source/dcell/cell.d
+++ source/dcell/cell.d
@@ -5,6 +5,8 @@
 
 module dcell.cell;
 
+import std.algorithm;
+import dcell.coord;
 import dcell.style;
 
 /** 
@@ -26,110 +28,40 @@ struct Cell
  * CellBuffer is a logical grid of cells containing content to display on screen.
  * It uses double buffering which can be used to reduce redrawing content on screen,
  * which can have a very noticeable impact on performance and responsiveness.
+ *
+ * It behaves something like a two-dimensional array, but offers some conveniences.
+ * Values returned from the indexing are constant, but new values can be assigned.
  */
 class CellBuffer
 {
-    int w; /// width
-    int h; /// height
-    Cell[] cells; /// current content - linear for performance
-    Cell[] prev; /// previous content - linear for performance
+    private Coord size;
+    private Cell[] cells; // current content - linear for performance
+    private Cell[] prev; // previous content - linear for performance
 
-    private int index(int x, int y)
+    private int index(Coord pos)
     {
-        return (y * w + x);
+        return (pos.y * size.x + pos.x);
+    }
+
+    private bool isLegal(Coord pos)
+    {
+        return ((pos.x >= 0) && (pos.y >= 0) && (pos.x < size.x) && (pos.y < size.y));
     }
 
     this(const int cols, const int rows)
     {
-        w = cols;
-        h = rows;
-        cells = new Cell[w * h];
-        prev = new Cell[w * h];
+        this(Coord(cols, rows));
+    }
+
+    this(Coord sz)
+    {
+        size = sz;
+        cells = new Cell[size.x * size.y];
+        prev = new Cell[size.x * size.y];
         for (int i = 0; i < cells.length; i++)
         {
             cells[i].width = 1;
             cells[i].text = " ";
-        }
-    }
-
-    /**
-     * Set content for the cell.
-     *
-     * Params:
-     *   x = column (0 is left most)
-     *   y = row (0 is top)
-     *   s = text (character) to display.  Note that only a single
-     *       character (including combining marks) is written.
-     *   st = style to apply
-     *   width = should be 1 for normal width, 2 for full width
-     *           or -1 to calculate from the text.
-     */
-    void set(int x, int y, string s, Style st, int width = -1)
-    {
-        Cell c = {text: s, style: st, width: width};
-        set(x, y, c);
-    }
-
-    /**
-     * Set content for the cell, preserving existing styling.
-     *
-     * Params:
-     *   x = column (0 is left most)
-     *   y = row (0 is top)
-     *   s = text (character) to display.  Note that only a single
-     *       character (including combining marks) is written.
-     */
-    void set(int x, int y, string s)
-    {
-        if ((x >= 0) && (x < w) && (y >= 0) && (y < h))
-        {
-            if (s == "" || s[0] < ' ')
-            {
-                // no control sequences or empty strings
-                s = " ";
-            }
-            // TODO: We need to calculate the width using East Asian Width
-            auto index = index(x, y);
-            cells[index].text = s;
-            cells[index].width = 1;
-        }
-    }
-
-    /**
-     * Set only the style for a given location.
-     * This does not change change the textual content.
-     */
-    void set(int x, int y, Style st)
-    {
-        if ((x >= 0) && (x < w) && (y >= 0) && (y < h))
-        {
-            cells[index(x, y)].style = st;
-        }
-    }
-
-    /**
-     * Set content for the cell.  
-     *
-     * Params:
-     *   x = column (0 is left most)
-     *   y = row (0 is top)
-     *   c = content to store for the cell.
-     */
-    void set(int x, int y, Cell c)
-    {
-        if ((x >= 0) && (x < w) && (y >= 0) && (y < h))
-        {
-            if (c.text == "" || c.text[0] < ' ')
-            {
-                // no control sequences or empty strings
-                c.text = " ";
-            }
-            if (c.width < 0 || c.width > 2)
-            {
-                // TODO: determine based on East Asian Width
-                c.width = 1;
-            }
-            cells[index(x, y)] = c;
         }
     }
 
@@ -142,18 +74,31 @@ class CellBuffer
      * Params:
      *   x = column (0 is left most)
      *   y = row (0 is top)
-     *   b = mark all dirty if true, or clean if false
      */
     bool dirty(int x, int y)
     {
-        if ((x >= 0) && (x < w) && (y >= 0) && (y < h))
+        return dirty(Coord(x, y));
+    }
+
+    /**
+     * Is a cell dirty?  Dirty means that the cell has some content
+     * or style change that has not been written to the terminal yet.
+     * Writes of identical content to what was last displayed do not
+     * cause a cell to become dirty -- only *different* content does.
+     *
+     * Params:
+     *   pos = coordinates of cell to check
+     */
+    bool dirty(Coord pos)
+    {
+        if (isLegal(pos))
         {
-            auto index = index(x, y);
-            if (prev[index].text == "")
+            auto ix = index(pos);
+            if (prev[ix].text == "")
             {
                 return true;
             }
-            return (cells[index] != prev[index]);
+            return cells[ix] != prev[ix];
         }
         return false;
     }
@@ -162,22 +107,21 @@ class CellBuffer
      * Mark a cell as either dirty or clean.
      *
      * Params:
-     *   x = column (0 is left most)
-     *   y = row (0 is top)
+     *   pos = coordinate of sell to update
      *   b = mark all dirty if true, or clean if false
      */
-    void setDirty(int x, int y, bool b)
+    void setDirty(Coord pos, bool b)
     {
-        if ((x >= 0) && (x < w) && (y >= 0) && (y < h))
+        if ((pos.x >= 0) && (pos.x < size.x) && (pos.y >= 0) && (pos.y < size.y))
         {
-            auto index = y * w + x;
+            auto ix = index(pos);
             if (b)
             {
-                prev[index].text = "";
+                prev[ix].text = "";
             }
             else
             {
-                prev[index] = cells[index];
+                prev[ix] = cells[ix];
             }
         }
     }
@@ -207,14 +151,88 @@ class CellBuffer
         }
     }
 
-    Cell get(int x, int y)
+    const(Cell) opIndex(Coord pos)
     {
-        if ((x >= 0) && (x < w) && (y >= 0) && (y < h))
+        if (isLegal(pos))
         {
-            return cells[index(x, y)];
+            return cells[index(pos)];
         }
-        Cell c;
-        return c;
+        return Cell();
+    }
+
+    const(Cell) opIndex(int x, int y)
+    {
+        return this[(Coord(x, y))];
+    }
+
+    /**
+     * Set content for the cell.  
+     *
+     * Params:
+     *   c = content to store for the cell.
+     *   pos = coordinate of the cell
+     */
+    void opIndexAssign(Cell c, Coord pos)
+    {
+        if (c.text == "" || c.text[0] < ' ')
+        {
+            c.text = " ";
+        }
+        // TODO: East Asian Width
+        c.width = 1;
+
+        if (isLegal(pos))
+        {
+            auto ix = index(pos);
+
+            cells[index(pos)] = c;
+        }
+    }
+
+    /**
+     * Set content for the cell, preserving existing styling.
+     *
+     * Params:
+     *   s = text (character) to display.  Note that only a single
+     *       character (including combining marks) is written.
+     *   pos = coordinate to update.
+     */
+    void opIndexAssign(string s, Coord pos)
+    {
+        if (s == "" || s[0] < ' ')
+        {
+            s = " ";
+        }
+        // TODO: East Asian Width
+        if (isLegal(pos))
+        {
+            auto ix = index(pos);
+            cells[ix].text = s;
+            cells[ix].width = 1;
+        }
+    }
+
+    void opIndexAssign(Style style, Coord pos)
+    {
+        if (isLegal(pos))
+        {
+            cells[index(pos)].style = style;
+        }
+    }
+
+    void opIndexAssign(Cell c, int x, int y)
+    {
+        this[Coord(x, y)] = c;
+    }
+
+    void opIndexAssign(string v, int x, int y)
+    {
+        this[Coord(x, y)] = v;
+    }
+
+    void opIndexAssign(Style v, int x, int y)
+    {
+        this[Coord(x, y)] = v;
     }
 
     void fill(Cell c)
@@ -262,13 +280,13 @@ class CellBuffer
      * content.  The entire set of contents are marked dirty, because
      * presumably everything needs to be redrawn when this happens.
      */
-    void resize(int cols, int rows)
+    void resize(Coord newSz)
     {
-        if (w == cols && h == rows)
+        if (size == newSz)
         {
             return;
         }
-        auto newCells = new Cell[cols * rows];
+        auto newCells = new Cell[newSz.x * newSz.y];
         for (int i = 0; i < newCells.length; i++)
         {
             // prefill with whitespace
@@ -276,20 +294,24 @@ class CellBuffer
             newCells[i].width = 1;
         }
         // maximum dimensions to copy (minimum of dimensions)
-        int lx = cols < w ? cols : w;
-        int ly = rows < h ? rows : h;
+        int lx = min(newSz.x, size.x);
+        int ly = min(newSz.y, size.y);
 
         for (int y = 0; y < ly; y++)
         {
             for (int x = 0; x < lx; x++)
             {
-                newCells[y * cols + x] = cells[y * h + x];
+                newCells[y * newSz.x + x] = cells[y * size.x + x];
             }
         }
-        h = rows;
-        w = cols;
+        size = newSz;
         cells = newCells;
-        prev = new Cell[cols * rows];
+        prev = new Cell[size.x * size.y];
+    }
+
+    void resize(int cols, int rows)
+    {
+        resize(Coord(cols, rows));
     }
 
     unittest
@@ -297,124 +319,124 @@ class CellBuffer
         auto cb = new CellBuffer(80, 24);
         assert(cb.cells.length == 24 * 80);
         assert(cb.prev.length == 24 * 80);
-        assert(cb.w == 80);
-        assert(cb.h == 24);
+        assert(cb.size == Coord(80, 24));
 
-        cb.set(2, 5, "b");
-        auto c = cb.get(2, 5);
+        cb[Coord(2, 5)] = "b";
+        assert(cb[2, 5].text == "b");
+        Cell c = cb[2, 5];
         Style st;
         assert(c.width == 1);
         assert(c.text == "b");
         assert(c.style == st);
-        assert(cb.cells[5 * cb.w + 2].text == "b");
+        assert(cb.cells[5 * cb.size.x + 2].text == "b");
 
         st.bg = Color.white;
         st.fg = Color.blue;
         st.attr = Attr.reverse;
-        cb.set(3, 5, "z", st);
+        cb[3, 5] = "z";
+        cb[3, 5] = st;
 
-        c = cb.get(3, 5);
+        c = cb[Coord(3, 5)];
         assert(c.style.bg == Color.white);
         assert(c.style.fg == Color.blue);
         assert(c.style.attr == Attr.reverse);
 
-        cb.set(-1, 100, "z", st);
-        cb.set(1, -100, "z", st);
+        cb[Coord(-1, 100)] = "z";
+        cb[Coord(-1, 100)] = st;
+        //cb.set(Coord(1, -100), "z", st);
 
-        c = cb.get(1, -100);
+        c = cb[Coord(1, -100)];
         assert(c.style.bg == Color.none);
         assert(c.style.fg == Color.none);
         assert(c.style.attr == Attr.none);
 
-        cb.set(0, 0, "", st);
-        c = cb.get(0, 0);
+        cb[0, 0] = Cell("", st, 0);
+        c = cb[0, 0];
         assert(c.text == " "); // space replaces null string
         assert(c.width == 1);
         assert(c.style == st);
 
-        cb.set(1, 0, "\x1b", st);
-        c = cb.get(1, 0);
+        cb[1, 0] = Cell("\x1b", st, 0);
+        c = cb[1, 0];
         assert(c.text == " "); // space replaces control sequence
         assert(c.width == 1);
         assert(c.style == st);
 
-        cb.set(1, 1, "\x1b");
-        c = cb.get(1, 1);
+        cb[1, 1] = "\x1b";
+        c = cb[1, 1];
         assert(c.text == " "); // space replaces control sequence
         assert(c.width == 1);
 
         c.text = "@";
-        cb.set(2, 0, c);
-        c = cb.get(2, 0);
+        cb[2, 0] = c;
+        c = cb[2, 0];
         assert(c.text == "@");
         assert(cb.dirty(2, 0));
 
-        cb.set(-1, 10, c);
-        c = cb.get(-1, 10);
+        cb[-1, 10] = c;
+        c = cb[-1, 10];
         assert(c.text == "");
         assert(c.style.bg == Color.none);
         assert(c.style.fg == Color.none);
         assert(c.style.attr == Attr.none);
         assert(!cb.dirty(-1, 10));
 
-        cb.set(1, -10, c);
-        c = cb.get(1, -10);
-        assert(c.text == "");
+        cb[Coord(1, -10)] = c; // invalid
+        c = cb[Coord(1, -10)];
+        //assert(c.text == "");
         assert(c.style.bg == Color.none);
         assert(c.style.fg == Color.none);
         assert(c.style.attr == Attr.none);
-        assert(!cb.dirty(1, -10));
+        assert(!cb.dirty(Coord(1, -10)));
 
         st.attr = Attr.reverse;
         st.bg = Color.none;
         st.fg = Color.maroon;
         cb.fill("%", st);
-        assert(cb.get(1, 1).text == "%");
-        assert(cb.get(1, 1).style == st);
+        assert(cb[1, 1].text == "%");
+        assert(cb[1, 1].style == st);
         cb.fill("s");
 
         cb.setAllDirty(false);
 
-        cb.set(1, 1, "U");
-        cb.set(1, 1, st);
-        assert(cb.get(1, 1).style == st);
-        assert(cb.get(1, 1).text == "U");
+        cb[1, 1] = "U";
+        cb[1, 1] = st;
+        assert(cb[1, 1].style == st);
+        assert(cb[1, 1].text == "U");
         assert(cb.dirty(1, 1));
         assert(!cb.dirty(2, 1));
 
         assert(cb.prev[0] == cb.cells[0]);
 
-        cb.setDirty(2, 1, true);
+        cb.setDirty(Coord(2, 1), true);
         assert(cb.dirty(2, 1));
         assert(!cb.dirty(3, 1));
 
-        cb.setDirty(3, 1, false);
-        assert(!cb.dirty(3, 1));
+        cb.setDirty(Coord(3, 1), false);
+        assert(!cb.dirty(Coord(3, 1)));
         cb.setAllDirty(true);
-        assert(cb.dirty(3, 1));
+        assert(cb.dirty(Coord(3, 1)));
 
         c.width = -1;
         c.text = "A";
         cb.fill(c);
-        assert(cb.get(0, 0).width == 1);
-        assert(cb.get(0, 0).text == "A");
-        assert(cb.get(1, 23).text == "A");
-        assert(cb.get(79, 23).text == "A");
+        assert(cb[0, 0].width == 1);
+        assert(cb[0, 0].text == "A");
+        assert(cb[1, 23].text == "A");
+        assert(cb[79, 23].text == "A");
         cb.resize(132, 50);
-        assert(cb.w == 132);
-        assert(cb.h == 50);
-        assert(cb.get(79, 23).text == "A");
-        assert(cb.get(80, 23).text == " ");
-        assert(cb.get(79, 24).text == " ");
+        assert(cb.size == Coord(132, 50));
+        assert(cb[79, 23].text == "A");
+        assert(cb[80, 23].text == " ");
+        assert(cb[79, 24].text == " ");
         cb.resize(132, 50); // this should be a no-op
-        assert(cb.w == 132);
-        assert(cb.h == 50);
-        assert(cb.get(79, 23).text == "A");
-        assert(cb.get(80, 23).text == " ");
-        assert(cb.get(79, 24).text == " ");
+        assert(cb.size == Coord(132, 50));
+        assert(cb[79, 23].text == "A");
+        assert(cb[80, 23].text == " ");
+        assert(cb[79, 24].text == " ");
 
         c.text = "";
         cb.fill(c);
-        assert(cb.get(79, 23).text == " ");
+        assert(cb[79, 23].text == " ");
     }
 }
