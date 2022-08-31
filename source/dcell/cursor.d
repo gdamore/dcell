64d3d35e8befafedfb45bc93c31e835cb16a0e51
Garrett D'Amore <garrett@damore.org>
2022-08-31 14:52:06 -0700

    Add missing cursor module.

---
+++ source/dcell/cursor.d
@@ -0,0 +1,24 @@
+// Copyright 2022 Garrett D'Amore
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or https://www.boost.org/LICENSE_1_0.txt)
+
+module dcell.cursor;
+
+import core.time;
+
+/** 
+ * The buttons that may be clicked, etc. on a mouse.  These can be cmobined
+ * together as a binary value to represent chording.  Scroll wheels are
+ * included.
+ */
+enum Cursor
+{
+    normal = 0, // default for normal cursor
+    block,
+    underline,
+    bar,
+    blinkingBlock,
+    blinkingUnderline,
+    blinkingBar,
+}
